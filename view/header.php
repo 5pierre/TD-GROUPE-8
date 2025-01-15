0d80980c001aa5f262cd5a9cfe17fc39907164db
frederick <frederick.rat@efrei.net>
2025-01-15 19:16:37 +0100

    Commit style header

--- view/header.php
+++ view/header.php
@@ -5,37 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cat hub</title>
 
-    <link rel="stylesheet" href="https://bootswatch.com/5/minty/bootstrap.css">
+    <link rel="stylesheet" href="view/styles/styleheader.css">
 
 </head>
 <body>
  
-<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-  <div class="container-fluid me-2">
-    <a href="?page=acceuil">
-      <img src="view/images/Cathublogo.png" alt="logo cat hub" height="50px">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item me-2">
-          <a class="btn btn-light ms-auto" href="?page=acceuil">Accueil</a>
+<nav class="navbar" data-bs-theme="dark">
+  <div class="container-fluid">
+    
+ 
+
+    <div class="collapse" id="navbarColor01">
+
+      <a href="?page=acceuil">
+        <img src="view/images/Cathublogo.png" alt="logo cat hub">
+      </a>
+      <ul class="navbar-nav">
+
+        <li class="nav-item">
+          <a class="btn" href="?page=acceuil">Accueil</a>
         </li>
-        <li class="nav-item me-2">
-          <a class="btn btn-light ms-auto" href="?page=contact">Contact</a>
+
+        <li class="nav-item">
+          <a class="btn" href="?page=contact">Contact</a>
         </li>
-        <li class="nav-item me-2">
-          <a class="btn btn-light ms-auto" href="?page=video">Video</a>
+
+        <li class="nav-item">
+          <a class="btn" href="?page=video">Video</a>
         </li>
-        <li class="nav-item me-2">
-          <a class="btn btn-light ms-auto" href="?page=faq">FAQ</a>
+
+        <li class="nav-item">
+          <a class="btn" href="?page=faq">FAQ</a>
         </li>
+
       </ul>
-      <a class="btn btn-light ms-auto" href="?page=login">Login</a>
+
+      <a class="btn" href="?page=login">
+        <img src="view/images/icon.jpg" alt="">
+      </a>
+
     </div>
   </div>
 </nav>
-
-
